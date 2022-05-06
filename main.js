def0fe8fea2f936912a54321c3c4420da730685d
neffoxxx <artemkampod4@gmail.com>
2022-05-06 14:50:06 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+document.write('<h1>Константи</h1>');
+document.write('Значення константи е = ', Math.E, '<br>');
+document.write('Значення константи π = ', Math.PI, '<br>');
+document.write('Десятковий логарифм постійної Ейлера (е) = ', Math.LOG10E, '<br>');
+document.write('Двійковий логарифм постійної Ейлера (е) = ', Math.LOG2E, '<br>');
+document.write('Натуральний логарифм числа 10 = ', Math.LN10, '<br>');
+document.write('Натуральний логарифм числа 2 = ', Math.LN2, '<br>');
+document.write('Корінь квадратний із двох = ', Math.SQRT2, '<br>');
+document.write('Корінь квадратний з однієї другий = ', Math.SQRT1_2, '<br>');
+document.write('<h1>Функції</h1>');
+document.write('10. Результат обчислення арксинуса одиниці = ', Math.asin(1), '<br>');
+document.write('11. Тангенс 45 градусів (π /4 радіан) = ', Math.tan(Math.PI/4), '<br>');
+document.write('12. Результат обчислення арктангенса одиниці = ', Math.atan(1), ' rad' + '<br>');
+document.write('13. Результат зведення е в ступінь 2 = ', Math.exp(2), '<br>');
+document.write('14. Натуральний логарифм від e^2 = ', Math.log10(Math.exp(2)), '<br>');
+document.write('<h1>Генератор випадкових чисел</h1>');
+document.write('Формування випадкових чисел в інтервалі [0;1] = ', Math.random(), '<br>');
+
